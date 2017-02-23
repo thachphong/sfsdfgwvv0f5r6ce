31ac4adec001a6ad429970f14641fd23a22915ba
thachphong <whirlwind2887@gmail.com>
2017-02-23 22:46:44 +0700

    fix log

--- QLNhiemvu_WebAPI/Functions/index.aspx.cs
+++ QLNhiemvu_WebAPI/Functions/index.aspx.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.IO;
 namespace QLNhiemvu_WebAPI.Functions
 {
     public partial class index : System.Web.UI.Page
@@ -14,6 +14,12 @@ namespace QLNhiemvu_WebAPI.Functions
         {
             using (DataTools dataTools = new DataTools())
             {
+                string path = HttpContext.Current.Server.MapPath("~");
+                string folder_name = path + @"bin\log";
+                bool folderExists =  Directory.Exists(folder_name);
+                if (!folderExists)
+                    Directory.CreateDirectory(folder_name);
+
                 pDBConnection.InnerHtml = dataTools.DBMaster_CheckConnection() ? "OK" : "LOST";
             }
         }
--- QLNhiemvu_WebAPI/Log.cs
+++ QLNhiemvu_WebAPI/Log.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-
+using System.Web;
 namespace QLNhiemVu
 {
     class Log
     {
         public static void write(Exception ex)
         {
-            string filePath = @"log\ERROR_" + DateTime.Now.ToString("yyyyMMdd") +".log";
-
+            string path = HttpContext.Current.Server.MapPath("~");
+            string filePath = path +  @"bin\log\ERROR_" + DateTime.Now.ToString("yyyyMMdd") +".log";
+            
             StreamWriter writer = new StreamWriter(filePath , true);            
                 writer.WriteLine("Message :" + ex.Message + "<br/>" + Environment.NewLine + "StackTrace :" + ex.StackTrace +
                    "" + Environment.NewLine + "Date :" + DateTime.Now.ToString());
